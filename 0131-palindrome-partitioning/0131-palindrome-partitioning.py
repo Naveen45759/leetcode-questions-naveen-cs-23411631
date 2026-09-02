3aa6d417a5299fea33123e7283e09e5a96179941
Naveen45759 <naveenkr.5805@gmail.com>
2026-09-02 11:29:35 +0530

    Time: 76 ms (65.58%), Space: 47.9 MB (61.17%) - LeetHub-Neo

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def partition(self, s):
+        result = []
+
+        def backtrack(start, path):
+            if start == len(s):
+                result.append(path[:])
+                return
+
+            for end in range(start, len(s)):
+                part = s[start:end + 1]
+
+                if part == part[::-1]:
+                    path.append(part)
+
+                    backtrack(end + 1, path)
+
+                    path.pop()
+
+        backtrack(0, [])
+        return result
