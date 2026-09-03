fe4424ce3e7dc37e0afc84c0a58c6eef8aecde26
Naveen45759 <naveenkr.5805@gmail.com>
2026-09-03 09:44:56 +0530

    Time: 51 ms (30.38%) | Memory: 13.7 MB (22.78%) - LeetSync

--- 1216-print-zero-even-odd/print-zero-even-odd.py
+++ 1216-print-zero-even-odd/print-zero-even-odd.py
@@ -11,30 +11,22 @@ class ZeroEvenOdd:
 
     def zero(self, printNumber):
         for i in range(1, self.n + 1):
-
             self.zero_sem.acquire()
-
             printNumber(0)
 
-            if i % 2 == 1:
+            if i % 2:
                 self.odd_sem.release()
             else:
                 self.even_sem.release()
 
-    def even(self, printNumber):
-        for i in range(2, self.n + 1, 2):
-
-            self.even_sem.acquire()
-
-            printNumber(i)
-
-            self.zero_sem.release()
-
     def odd(self, printNumber):
         for i in range(1, self.n + 1, 2):
-
             self.odd_sem.acquire()
-
             printNumber(i)
+            self.zero_sem.release()
 
+    def even(self, printNumber):
+        for i in range(2, self.n + 1, 2):
+            self.even_sem.acquire()
+            printNumber(i)
             self.zero_sem.release()
