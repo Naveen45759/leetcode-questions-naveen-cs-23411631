1702f7202157090b1882350de94f3c0ee111c8e7
Naveen45759 <naveenkr.5805@gmail.com>
2026-09-03 10:21:29 +0530

    Time: 37 ms (86.02%) | Memory: 12.4 MB (98.92%) - LeetSync

--- 1187-print-foobar-alternately/print-foobar-alternately.py
+++ 1187-print-foobar-alternately/print-foobar-alternately.py
@@ -4,17 +4,17 @@ class FooBar:
 
     def __init__(self, n):
         self.n = n
-        self.foo_turn = Semaphore(1)
-        self.bar_turn = Semaphore(0)
+        self.foo_sem = threading.Semaphore(1)
+        self.bar_sem = threading.Semaphore(0)
 
     def foo(self, printFoo):
         for _ in range(self.n):
-            self.foo_turn.acquire()
-            printFoo()
-            self.bar_turn.release()
+           self.foo_sem.acquire()
+           printFoo()
+           self.bar_sem.release()
 
     def bar(self, printBar):
         for _ in range(self.n):
-            self.bar_turn.acquire()
+            self.bar_sem.acquire()
             printBar()
-            self.foo_turn.release()
+            self.foo_sem.release()
